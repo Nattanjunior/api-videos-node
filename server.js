973855d4808465463ac30daa847cfc1824dd82ad
nattanjunior <fnatanieljunior@gmail.com>
2024-07-03 20:05:06 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,52 @@
+import { fastify } from 'fastify'
+import { DatebaseMemory } from './database-memory.js';
+import { DatebasePostgres } from './database-postgres.js';
+
+const server = fastify();
+
+const database = new DatebasePostgres();
+
+server.post('/videos', async (req, res) => {
+    const { title, description, duration } = req.body
+
+    await database.create({
+        title,
+        description,
+        duration
+    })
+    return res.status(201).send()
+})
+
+server.get('/videos', async (req,res) => {
+    const search = req.query.search
+
+    const videos = await database.list(search)
+    return videos
+})
+
+server.put('/video/:id', async (req,res) => {
+    const videoId = req.params.id
+    const { title, description, duration } = req.body
+
+    await database.update(videoId,{
+        title,
+        description,
+        duration
+    })
+
+    return res.status(204).send()
+
+})
+
+server.delete('/video/:id', async (req, res) => {
+    const videoId = req.params.id    
+    await database.delete(videoId)
+    
+    return res.status(204).send()
+})
+
+
+
+server.listen({
+    port: process.env.PORT ?? 3000
+})
